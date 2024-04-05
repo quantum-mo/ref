74da9c3a50b5a1cb7c631859a93c58d6a5a3b973
hamoo <hamoo@LAPTOP-L2MAIGCB>
2024-04-05 11:48:11 +0300

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        String img1_str = "wow.jpg";
+        String img2_str = "wow2.jpg";
+        String img3_str = "wow3.jpg";
+        String img4_str = "wow4.jpg";
+
+        List<student> students = new List<student>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Clicked(object sender, EventArgs e)
+        {
+            Stu_List_View.ItemsSource = students;
+            students.Add(new student());
+            Stu_List_View.ItemsSource = null;
+            Stu_List_View.ItemsSource = students;
+        }
+    }
+
+}
---
+++ App1/student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class student
+    {
+
+        public static int counter = 0;
+        public String name {  get; set; }  
+        public int ID { get; set; } 
+        public int fD { get; set; } 
+        public int fD { get; set; } 
+        public int ID { get; set; } 
+        public int ID { get; set; } 
+        
+        public student() { }
+
+        public student(String name) {
+            counter++;
+            ID = counter;
+            this.name = name;
+        
+        }public student(String name) {
+            counter++;
+            ID = counter;
+            this.name = name;
+        
+        }
+    }
+}
